d707588f329bb5113b4576630d584f27245f0009
bbooss97 <bbooss97@gmail.com>
2022-03-13 22:49:39 +0100

    log

--- applicazione/backend/server.js
+++ applicazione/backend/server.js
@@ -3,39 +3,100 @@ var app = express();
 var fs=require('fs');
 var request= require("request")
 var bodyParser = require('body-parser')
+const session = require('express-session');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+app.use(session({
+  secret: 'token segreto',
+  resave: false,
+  saveUninitialized: true,
+  unset: 'destroy',
+  store: store,
+  name: 'logged',
+  genid: (req) => {
+      // Restituire un ID identificativo casuale per la sessione
+  }
+}));
 
-// app.get('/news', function (req, res) {
-//   //res.send('Hello World!');
-//   console.log(req.query)
-//   argomento=req.query.argomento
-//   articoli=cercaNews(argomento,res)
-// });
+var mysql = require('mysql');
 
-// app.get('/newsNonFake', function (req, res) {
+var con = mysql.createConnection({
+  host: "sql-db-1",
+  database: "mysql",
+  port: "3306",
+  user: "root",
+  password: "example"
+});
+a=1
 
 
-app.get("*",function (req, res) {
-    url=req.url
-    console.log(url)
-    if(url=="/"){
-        res.sendFile(__dirname+"/index.html")
-        return;
-    }
-    res.sendFile(__dirname+url)
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+function esegui(sql){
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
+}
+esegui("create table if not exists users(username text,email text,password text);")
+
+
+// app.get("*",function (req, res) {
+//     url=req.url
+//     console.log(url)
+//     if(url=="/"){
+//         res.sendFile(__dirname+"/index.html")
+//         return;
+//     }
+//     res.sendFile(__dirname+url)
+// })
+
+app.get("/",function (req, res) {
+  if(!req.session.user) {
+    res.redirect('/login');
+  } else {
+    res.sendFile(__dirname+"/index.html")
+}
+  
 })
-// app.get("/script.js")
 
-// app.post('/news', function (req, res) {
-//   articoli=req.body
-//   console.log(articoli)
-//   res.send("ok")
-//   //madare articoli nella queue
-//   sendToQueue(articoli)
-// });
+app.post('/register', async (req, res) => {
+
+  //esegui("inserisci in tabeella users")
+  
+});
+
+app.post('/login', async (req, res) => {
+  try {
+      let user = await db.users.findOne({email: req.body.email});
+      if(user !== null) {
+          req.session.user = {
+                email: user.email,
+                name: user.name
+          };
+          res.redirect('/');
+      } else {
+         // Login non valido
+      }
+  } catch(err) {
+      res.sendStatus(500);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  if(req.session.user) {
+      delete req.session.user;
+      res.redirect('/login');
+  } else {
+      res.redirect('/');
+  }        
+});
+
 app.listen(3000);
 
 
